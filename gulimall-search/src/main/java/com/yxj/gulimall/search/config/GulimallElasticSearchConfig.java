9b82b8b2cfa25157eabd26386ed2aef7ff12d2d6
“zhanglong” <416095373@qq.com>
2022-04-15 23:59:42 +0800

    项目初始化

---
+++ gulimall-search/src/main/java/com/yxj/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -0,0 +1,38 @@
+package com.yxj.gulimall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yaoxinjia
+ */
+@SpringBootConfiguration
+public class GulimallElasticSearchConfig {
+
+    @Value("${elasticsearch.url}")
+    private String url;
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost(url, 9200, "http")
+                )
+        );
+        return client;
+    }
+}
